025471107d825dc18e1590ebd080d6e7c59506ba
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-16 23:09:43 -0300

    Atualizacao de chave de persistencia

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'gobarber',
+      key: 'fastfett',
       storage,
       whitelist: ['auth', 'user'],
     },
