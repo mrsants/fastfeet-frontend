9b4773da80ff8caaeb19ab16ccc06ccc7a92a479
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-28 19:20:21 -0300

    Refactor para function component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,109 +1,88 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import logo from "../../assets/fastfeet-logo.png";
-import history from "../../services/history";
-import * as Actions from "../../store/modules/auth/actions";
-import { MenuItem, Config, Container, Menu } from "./styles";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import logo from '../../assets/fastfeet-logo.png';
+import history from '../../services/history';
+import { Config, Container, Menu, MenuItem } from './styles';
+import { signOut } from '../../store/modules/auth/actions';
 
-class Header extends Component {
-  state = {
-    menu: {
-      controls: [
-        {
-          id: 0,
-          description: "ENCOMENDAS",
-          path: "/orders"
-        },
-        {
-          id: 1,
-          description: "ENTREGADORES",
-          path: "/deliverymans"
-        },
-        {
-          id: 2,
-          description: "DESTINATÁRIOS",
-          path: "/recipient"
-        },
-        {
-          id: 3,
-          description: "PROBLEMAS",
-          path: "/problems"
-        }
-      ]
+export default function Header() {
+  const [controls, setControls] = useState([
+    {
+      id: 0,
+      description: 'ENCOMENDAS',
+      path: '/orders',
     },
-    font: {
-      color: "#fff",
-      index: 0
-    }
-  };
+    {
+      id: 1,
+      description: 'ENTREGADORES',
+      path: '/deliverymans',
+    },
+    {
+      id: 2,
+      description: 'DESTINATÁRIOS',
+      path: '/recipient',
+    },
+    {
+      id: 3,
+      description: 'PROBLEMAS',
+      path: '/problems',
+    },
+  ]);
 
-  redirectTo() {
-    const { signOut } = this.props;
-    signOut();
+  const [fontIndex, setFontIndex] = useState(0);
+  const auth = useSelector(state => state.auth);
+
+  const dispatch = useDispatch();
+
+  function handleRedirectTo() {
+    history.push('/orders');
+    setFontIndex(0);
   }
 
-  componentDidMount() {
-    let updatePath = this.state.menu.controls.filter((menuFindItem, index) => {
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  useEffect(() => {
+    const updatePath = controls.filter((menuFindItem, index) => {
       return menuFindItem.path === history.location.pathname;
     });
 
-    this.setState({
-      font: {
-        index: updatePath[0] && updatePath[0].id
-      }
-    });
-  }
+    if (updatePath) {
+      setFontIndex(updatePath[0].id);
+    }
+  }, [controls]);
+  return (
+    <Container>
+      <Menu>
+        <div onClick={() => handleRedirectTo()}>
+          <img src={logo} alt="Logo Fastfeet" />
+        </div>
+        {controls.map((menuItem, index) => {
+          return (
+            <MenuItem
+              key={menuItem.id}
+              index={fontIndex}
+              className={`bold-${index}`}
+              onClick={e => {
+                e.preventDefault();
+                setFontIndex(index);
+                history.push(menuItem.path);
+              }}
+            >
+              {menuItem.description}
+            </MenuItem>
+          );
+        })}
+      </Menu>
 
-  render() {
-    const { auth } = this.props;
-    const { menu } = this.state;
-
-    return (
-      <Container>
-        <Menu>
-          <img
-            src={logo}
-            alt="Logo Fastfeet"
-            onClick={() => {
-              history.push("/orders");
-            }}
-          />
-          {menu.controls.map((menuItem, index) => {
-            return (
-              <MenuItem
-                key={menuItem.id}
-                index={this.state.font.index}
-                className={`bold-${index}`}
-                onClick={e => {
-                  e.preventDefault();
-                  this.setState({
-                    font: {
-                      index
-                    }
-                  });
-                  history.push(menuItem.path);
-                }}
-              >
-                {menuItem.description}
-              </MenuItem>
-            );
-          })}
-        </Menu>
-
-        <Config>
-          <strong>{auth.user.name}</strong>
-          <span onClick={() => this.redirectTo()}>sair do sistema</span>
-        </Config>
-      </Container>
-    );
-  }
+      <Config>
+        <strong>{auth.user.name}</strong>
+        <span onClick={() => handleSignOut()}>sair do sistema</span>
+      </Config>
+    </Container>
+  );
 }
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
