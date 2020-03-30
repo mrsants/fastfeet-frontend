f8546020b899ce74e42b56ec666bb8f8bc400701
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-03-30 19:39:32 -0300

    Implementacao de metodo para deletar

--- src/pages/Recipients/PopoverRecipientUi/index.js
+++ src/pages/Recipients/PopoverRecipientUi/index.js
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { MdDeleteForever, MdEdit } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
-import { recipientsUpdate } from '../../../store/modules/recipients/actions';
+import { recipientsDelete, recipientsUpdate } from '../../../store/modules/recipients/actions';
 
 export default function PopoverRecipientUi({ data }) {
   const dispatch = useDispatch();
 
-  function handleRedirectTo() {
+  function handleUpdate() {
     dispatch(
       recipientsUpdate({
         ...data,
@@ -16,14 +16,18 @@ export default function PopoverRecipientUi({ data }) {
     );
   }
 
+  function handleDelete(param) {
+    dispatch(recipientsDelete(param));
+  }
+
   return (
     <>
-      <div onClick={() => handleRedirectTo()}>
+      <div onClick={() => handleUpdate()}>
         <MdEdit size="16" color="#4D85EE" />
         <span>Editar</span>
       </div>
       <hr />
-      <div>
+      <div onClick={()=> handleDelete(data.id)}>
         <MdDeleteForever size="16" color="#DE3B3B" />
         <span>Excluir</span>
       </div>
